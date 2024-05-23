dfc5b8ba7850e03b7bb510e9a6f90411d914b618
binhsna <99773978+binhsna@users.noreply.github.com>
2024-05-23 18:24:13 +0700

    GameCaroAI

---
+++ GameCaroAI/src/gameCaro/dao/ActivityLog.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package gameCaro.dao;
+
+/**
+ * Lưu kết quả thắng thua của User và AI
+
+ */
+public class ActivityLog {
+
+}
---
+++ GameCaroAI/src/gameCaro/dao/SettingDao.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package gameCaro.dao;
+
+import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+
+import gameCaro.bean.Setting;
+import gameCaro.values.Value;
+
+
+/**
+ * Lấy/ Cập nhật thông tin setting của game
+ */
+public class SettingDao {
+	public static Setting LoadSettingInfo() {
+		File file = new File("file\\setting.txt");
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (Exception e1) {
+				e1.printStackTrace();
+				System.out.println("Có lỗi khi mở file setting.txt!");
+				return new Setting(Value.BACKGROUND_COLOR, Value.CELL_COLOR, Value.USER_TEXT_COLOR, Value.AI_TEXT_COLOR, Value.DEFAULT_MODE);
+			}
+		}
+		try {
+			// Load data unicode
+			BufferedReader reader = 
+				new BufferedReader(new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
+			String[] rem = reader.readLine().split("[,]");
+
+			Color backgroundColor = new Color(Integer.parseInt(rem[0]), Integer.parseInt(rem[1]), Integer.parseInt(rem[2]));
+			rem = reader.readLine().split("[,]");
+			Color cellColor = new Color(Integer.parseInt(rem[0]), Integer.parseInt(rem[1]), Integer.parseInt(rem[2]));
+			rem = reader.readLine().split("[,]");
+			Color xColor = new Color(Integer.parseInt(rem[0]), Integer.parseInt(rem[1]), Integer.parseInt(rem[2]));
+			rem = reader.readLine().split("[,]");
+			Color oColor = new Color(Integer.parseInt(rem[0]), Integer.parseInt(rem[1]), Integer.parseInt(rem[2]));
+			int mode = Integer.parseInt(reader.readLine());
+			Setting setting = new Setting(backgroundColor, cellColor, xColor, oColor, mode);
+			reader.close();
+			return setting;
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Có lỗi khi mở file setting.txt!");
+			return new Setting(Value.BACKGROUND_COLOR, Value.CELL_COLOR, Value.USER_TEXT_COLOR, Value.AI_TEXT_COLOR, Value.DEFAULT_MODE);
+		}
+	}
+	
+	public static void SaveSettingInfo(Setting setting) {
+		File file = new File("file\\setting.txt");
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (Exception e1) {
+				System.out.println("File không tồn tại!. Đã tạo mới file setting.txt!");
+			}
+		}
+		try {
+			// Save data unicode
+			OutputStreamWriter writer =
+	             new OutputStreamWriter(new FileOutputStream(file, false), StandardCharsets.UTF_8);
+			writer.write(setting.getStringBackgroundColor() + "\n");
+			writer.write(setting.getStringCellColor() + "\n");
+			writer.write(setting.getStringXColor() + "\n");
+			writer.write(setting.getStringOColor() + "\n");
+			writer.write(String.valueOf(setting.getMode()) + "\n");
+//			Color rem = setting.getTextStoryColor();
+//			String color = String.valueOf(rem.getRed())+","+String.valueOf(rem.getGreen())+","+String.valueOf(rem.getBlue());
+//			writer.write(color + "\n");
+			writer.close();
+		} catch (Exception e1) {
+			System.out.println("Có lỗi khi lưu file setting.txt!");
+		}
+	}
+}
