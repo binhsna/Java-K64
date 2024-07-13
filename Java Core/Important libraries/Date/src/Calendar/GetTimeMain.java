3606515df93e42d4f7b453443707d4fe9a18bf5e
binhsna <99773978+binhsna@users.noreply.github.com>
2024-07-13 09:30:30 +0700

    Date libraries

---
+++ Java Core/Important libraries/Date/src/Calendar/GetTimeMain.java
@@ -0,0 +1,22 @@
+package Calendar;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class GetTimeMain {
+    public static void main(String[] args) {
+        // Tạo đối tượng Calendar mô tả thời điểm hiện tại
+        // với Locale (khu vực) và TimeZone (múi giờ)
+        // của máy tính đang chạy.
+        Calendar cal = Calendar.getInstance();
+
+        // import java.util.Date; để sử dụng Date
+        // hiển thị ngày tháng năm giờ phút giây hiện tại của hệ thống
+        // sử dụng phương thức getTime()
+        // phương thức này sẽ trả về 1 đối tượng Date
+        // lưu trữ thông tin ngày tháng năm giờ phút giây hiện tại của hệ thống
+        Date date = cal.getTime();
+        // hiển thị thời gian hiện tại
+        System.out.println("Thời gian hiện tại của hệ thống là: " + date);
+    }
+}
---
+++ Java Core/Important libraries/Date/src/Calendar/RollMethod.java
@@ -0,0 +1,19 @@
+package Calendar;
+
+import java.util.Calendar;
+
+public class RollMethod {
+    public static void main(String[] args) {
+        Calendar cal = Calendar.getInstance();
+        System.out.println("Thời gian hiện tại là " + cal.getTime());
+
+        // thay đổi tháng thành 12
+        cal.set(Calendar.MONTH, Calendar.DECEMBER);
+        System.out.println("Thời gian hiện tại sau khi thay đổi là " + cal.getTime());
+
+        // tăng tháng sử dụng phương thức roll()
+        // lúc này tháng sẽ là tháng 1 và năm vẫn giữ nguyên
+        cal.roll(Calendar.MONTH, 1);
+        System.out.println("Thời gian hiện tại sau khi thay đổi là " + cal.getTime());
+    }
+}
