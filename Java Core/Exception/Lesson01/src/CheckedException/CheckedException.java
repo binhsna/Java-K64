b8c76c57637db84917e8b5105873b7eb135d82d4
binhsna <99773978+binhsna@users.noreply.github.com>
2024-08-07 08:33:44 +0700

    Exception - lesson 01

---
+++ Java Core/Exception/Lesson01/src/CheckedException/CheckedException.java
@@ -0,0 +1,7 @@
+package CheckedException;
+
+public class CheckedException {
+    public static void main(String[] args) {
+        System.out.println(ABC);
+    }
+}
---
+++ Java Core/Exception/Lesson01/src/UncheckedException/UncheckedException.java
@@ -0,0 +1,8 @@
+package UncheckedException;
+
+public class UncheckedException {
+    public static void main(String[] args) {
+        int a = 5, b = 0;
+        System.out.println(a / b);
+    }
+}
